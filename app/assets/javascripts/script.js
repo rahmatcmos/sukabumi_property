022ccbb2d2340329e1e94115a973c5d9cfc5289b
fathan <kreasi.fathan@gmail.com>
2014-12-07 11:21:54 +0700

    first commit

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,58 @@
+$('#myTab a').click(function (e) {
+	e.preventDefault()
+	$(this).tab('show')
+})
+
+// learn bootcamp jquery 1
+$(document).ready(function () {
+	$(".a").each(function() {
+		$(this).click(function (e) {
+			var detail = $(this).parents('.detail').find('.b');
+			e.preventDefault();
+			detail.slideDown(300);
+		});
+	});
+});
+
+// loading page
+$(document).ready(function () {
+		$(window).load(function() { $("#loading").fadeOut("slow"); 
+	});
+});
+
+// back to top
+$(function(){
+	$(document).on( 'scroll', function(){
+		if ($(window).scrollTop() > 100) {
+			$('.scroll-top-wrapper').addClass('show');
+		} else {
+			$('.scroll-top-wrapper').removeClass('show');
+		}
+	});
+	$('.scroll-top-wrapper').on('click', scrollToTop);
+});
+ 
+function scrollToTop() {
+	verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+	element = $('body');
+	offset = element.offset();
+	offsetTop = offset.top;
+	$('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+}
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+$(document).ready(function (){
+	var config = {
+      '.chosen-select'           : {},
+      '.chosen-select-deselect'  : {allow_single_deselect:true},
+      '.chosen-select-no-single' : {disable_search_threshold:10},
+      '.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
+      '.chosen-select-width'     : {width:"95%"}
+    }
+    for (var selector in config) {
+      $(selector).chosen(config[selector]);
+    }
+})
---
+++ app/controllers/publics_controller.rb
@@ -0,0 +1,29 @@
+class PublicsController < ApplicationController
+	
+	def home
+	end
+
+	def catalog
+	end
+
+	def single_post	
+	end
+
+	def about_us
+	end
+
+	def forum_diskusi
+	end
+
+	def contact
+	end
+
+	def questions
+	end
+
+	def sign_in
+	end
+
+	def sign_up
+	end
+end
